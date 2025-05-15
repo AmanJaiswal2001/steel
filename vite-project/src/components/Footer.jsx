552afe2a388e0babe35b50f37618abc7478da3f4
AmanJaiswal2001 <amanjaisawal537@gmail.com>
2025-05-15 18:29:20 +0530

    footer complte

--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -118,22 +118,86 @@ const Footer = () => {
 <img src="/Group 611.png" className='w-full' alt="Sonatek Logo" />
          
 </div>
+<div className='sm:hidden flex w-full justify-center  gap-5'>
+              <a target='_blank' href='https://www.youtube.com/channel/UCUmUCit141YxyIbTmjioJfA' className="link link-hover">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" width="24px" height="24px" fill="white">
+                  <g transform="scale(10.66667,10.66667)">
+                    <path d="M21.582,6.186c-0.23,-0.86 -0.908,-1.538 -1.768,-1.768c-1.56,-0.418 -7.814,-0.418 -7.814,-0.418c0,0 -6.254,0 -7.814,0.418c-0.86,0.23 -1.538,0.908 -1.768,1.768c-0.418,1.56 -0.418,5.814 -0.418,5.814c0,0 0,4.254 0.418,5.814c0.23,0.86 0.908,1.538 1.768,1.768c1.56,0.418 7.814,0.418 7.814,0.418c0,0 6.254,0 7.814,-0.418c0.861,-0.23 1.538,-0.908 1.768,-1.768c0.418,-1.56 0.418,-5.814 0.418,-5.814c0,0 0,-4.254 -0.418,-5.814zM10,14.598v-5.196c0,-0.385 0.417,-0.625 0.75,-0.433l4.5,2.598c0.333,0.192 0.333,0.674 0,0.866l-4.5,2.598c-0.333,0.193 -0.75,-0.048 -0.75,-0.433z"/>
+                  </g>
+                </svg>
+              </a>
+              <a target='_blank' href="https://www.instagram.com/sonateksteels/" className="link link-hover" aria-label="Instagram">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" width="24px" height="24px" fill="white">
+                  <g transform="scale(10.66667,10.66667)">
+                    <path d="M8,3c-2.761,0 -5,2.239 -5,5v8c0,2.761 2.239,5 5,5h8c2.761,0 5,-2.239 5,-5v-8c0,-2.761 -2.239,-5 -5,-5zM18,5c0.552,0 1,0.448 1,1c0,0.552 -0.448,1 -1,1c-0.552,0 -1,-0.448 -1,-1c0,-0.552 0.448,-1 1,-1zM12,7c2.761,0 5,2.239 5,5c0,2.761 -2.239,5 -5,5c-2.761,0 -5,-2.239 -5,-5c0,-2.761 2.239,-5 5,-5zM12,9c-1.65685,0 -3,1.34315 -3,3c0,1.65685 1.34315,3 3,3c1.65685,0 3,-1.34315 3,-3c0,-1.65685 -1.34315,-3 -3,-3z" />
+                  </g>
+                </svg>
+              </a>
+              <a target='_blank' href="https://www.facebook.com/people/Sonatek-Steels-Private-Limited/61557799228243/" className="link link-hover" aria-label="Facebook">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" width="24px" height="24px" fill="white">
+                  <g transform="scale(10.66667,10.66667)">
+                    <path d="M19,3h-14c-1.105,0 -2,0.895 -2,2v14c0,1.105 0.895,2 2,2h7.621v-6.961h-2.343v-2.725h2.343v-2.005c0,-2.324 1.421,-3.591 3.495,-3.591c0.699,-0.002 1.397,0.034 2.092,0.105v2.43h-1.428c-1.13,0 -1.35,0.534 -1.35,1.322v1.735h2.7l-0.351,2.725h-2.365v6.965h3.586c1.105,0 2,-0.895 2,-2v-14c0,-1.105 -0.895,-2 -2,-2z" />
+                  </g>
+                </svg>
+              </a>
+              <a target='_blank' href="https://www.linkedin.com/in/adish-jain-b10107310/?originalSubdomain=in" className="link link-hover" aria-label="LinkedIn">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" width="24px" height="24px" fill="white">
+                  <g transform="scale(10.66667,10.66667)">
+                    <path d="M19,3h-14c-1.105,0 -2,0.895 -2,2v14c0,1.105 0.895,2 2,2h14c1.105,0 2,-0.895 2,-2v-14c0,-1.105 -0.895,-2 -2,-2zM9,17h-2.523v-7h2.523zM7.694,8.717c-0.771,0 -1.286,-0.514 -1.286,-1.2c0,-0.686 0.514,-1.2 1.371,-1.2c0.771,0 1.286,0.514 1.286,1.2c0,0.686 -0.514,1.2 -1.371,1.2zM18,17h-2.442v-3.826c0,-1.058 -0.651,-1.302 -0.895,-1.302c-0.244,0 -1.058,0.163 -1.058,1.302v3.826h-2.523v-7h2.523v0.977c0.325,-0.57 0.976,-0.977 2.197,-0.977c1.221,0 2.198,0.977 2.198,3.174z" />
+                  </g>
+                </svg>
+              </a>
+            </div>
 </section>
 
      
       </footer>
      
 
-      <div className='h-16  w-full   bg-black  mx-auto flex items-center  justify-center'>
+      <div className='h-16  w-full sm:px-24     bg-black   flex sm:justify-between  items-center'>
+      <div className='sm:flex w-full hidden   gap-5'>
+              <a target='_blank' href='https://www.youtube.com/channel/UCUmUCit141YxyIbTmjioJfA' className="link link-hover">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" width="24px" height="24px" fill="white">
+                  <g transform="scale(10.66667,10.66667)">
+                    <path d="M21.582,6.186c-0.23,-0.86 -0.908,-1.538 -1.768,-1.768c-1.56,-0.418 -7.814,-0.418 -7.814,-0.418c0,0 -6.254,0 -7.814,0.418c-0.86,0.23 -1.538,0.908 -1.768,1.768c-0.418,1.56 -0.418,5.814 -0.418,5.814c0,0 0,4.254 0.418,5.814c0.23,0.86 0.908,1.538 1.768,1.768c1.56,0.418 7.814,0.418 7.814,0.418c0,0 6.254,0 7.814,-0.418c0.861,-0.23 1.538,-0.908 1.768,-1.768c0.418,-1.56 0.418,-5.814 0.418,-5.814c0,0 0,-4.254 -0.418,-5.814zM10,14.598v-5.196c0,-0.385 0.417,-0.625 0.75,-0.433l4.5,2.598c0.333,0.192 0.333,0.674 0,0.866l-4.5,2.598c-0.333,0.193 -0.75,-0.048 -0.75,-0.433z"/>
+                  </g>
+                </svg>
+              </a>
+              <a target='_blank' href="https://www.instagram.com/sonateksteels/" className="link link-hover" aria-label="Instagram">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" width="24px" height="24px" fill="white">
+                  <g transform="scale(10.66667,10.66667)">
+                    <path d="M8,3c-2.761,0 -5,2.239 -5,5v8c0,2.761 2.239,5 5,5h8c2.761,0 5,-2.239 5,-5v-8c0,-2.761 -2.239,-5 -5,-5zM18,5c0.552,0 1,0.448 1,1c0,0.552 -0.448,1 -1,1c-0.552,0 -1,-0.448 -1,-1c0,-0.552 0.448,-1 1,-1zM12,7c2.761,0 5,2.239 5,5c0,2.761 -2.239,5 -5,5c-2.761,0 -5,-2.239 -5,-5c0,-2.761 2.239,-5 5,-5zM12,9c-1.65685,0 -3,1.34315 -3,3c0,1.65685 1.34315,3 3,3c1.65685,0 3,-1.34315 3,-3c0,-1.65685 -1.34315,-3 -3,-3z" />
+                  </g>
+                </svg>
+              </a>
+              <a target='_blank' href="https://www.facebook.com/people/Sonatek-Steels-Private-Limited/61557799228243/" className="link link-hover" aria-label="Facebook">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" width="24px" height="24px" fill="white">
+                  <g transform="scale(10.66667,10.66667)">
+                    <path d="M19,3h-14c-1.105,0 -2,0.895 -2,2v14c0,1.105 0.895,2 2,2h7.621v-6.961h-2.343v-2.725h2.343v-2.005c0,-2.324 1.421,-3.591 3.495,-3.591c0.699,-0.002 1.397,0.034 2.092,0.105v2.43h-1.428c-1.13,0 -1.35,0.534 -1.35,1.322v1.735h2.7l-0.351,2.725h-2.365v6.965h3.586c1.105,0 2,-0.895 2,-2v-14c0,-1.105 -0.895,-2 -2,-2z" />
+                  </g>
+                </svg>
+              </a>
+              <a target='_blank' href="https://www.linkedin.com/in/adish-jain-b10107310/?originalSubdomain=in" className="link link-hover" aria-label="LinkedIn">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" width="24px" height="24px" fill="white">
+                  <g transform="scale(10.66667,10.66667)">
+                    <path d="M19,3h-14c-1.105,0 -2,0.895 -2,2v14c0,1.105 0.895,2 2,2h14c1.105,0 2,-0.895 2,-2v-14c0,-1.105 -0.895,-2 -2,-2zM9,17h-2.523v-7h2.523zM7.694,8.717c-0.771,0 -1.286,-0.514 -1.286,-1.2c0,-0.686 0.514,-1.2 1.371,-1.2c0.771,0 1.286,0.514 1.286,1.2c0,0.686 -0.514,1.2 -1.371,1.2zM18,17h-2.442v-3.826c0,-1.058 -0.651,-1.302 -0.895,-1.302c-0.244,0 -1.058,0.163 -1.058,1.302v3.826h-2.523v-7h2.523v0.977c0.325,-0.57 0.976,-0.977 2.197,-0.977c1.221,0 2.198,0.977 2.198,3.174z" />
+                  </g>
+                </svg>
+              </a>
+            </div>
+       
         <hr className=' bg-white'></hr>
-        <p className='text-white  font-poppins text-sm pt-2'>© Copyright 2025 Sonatek. Designed by</p>
+        <div className=' flex sm:justify-center px-2'>
+        <p className='text-white sm:w-72  font-poppins text-sm pt-2'>© Copyright 2025 Sonatek. Designed by</p>
         <a
           target='_blank'
           rel='noopener noreferrer'
           href='https://ganesyx.com/'
-          className='text-white font-bold font-poppins mx-2 text-sm pt-2 hover:underline'
+          className='text-white font-bold  font-poppins mx-1 text-sm pt-2 hover:underline'
         >Ganesyx
         </a>
+        </div>
+       
       </div>
     </div>
   )
